83fdee7a67a7b1d97063a90f734d775f4a787e8c
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2021-06-01 12:34:48 +1200

    basic php
    php tag
    echo
    variables
    if statement

---
+++ php-intro/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      echo "<h1>Hello World!</h1>";
+
+      // variables
+      $name = 'Ms Lee';
+      echo $name;
+
+      $name = "Mickey Mouse";
+      echo "<h2>$name</h2>";
+
+
+      $number = 1;
+
+      if ($number*10 > 40) {
+        // code to run if true goes here
+        echo "<p>Bigger than 40</p>";
+      }
+      else {
+        echo "<p>Smaller than or equal to 40</p>";
+      }
+
+
+     ?>
+
+
+  </body>
+</html>
