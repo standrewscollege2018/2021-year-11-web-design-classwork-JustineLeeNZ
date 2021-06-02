aa084e2a780bfc1bd9975b4150c4c13db6161c28
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2021-06-02 21:31:51 +1200

    FizzBuzz
    Displays numbers 1 to 100

---
+++ php-intro/fizzbuzz.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Fizzbuzz</title>
+  </head>
+  <body>
+    <!-- code to play Fizz Buzz -->
+    <?php
+      // loop from 1 to 100
+      $number = 1;
+
+      do {
+        // display current value
+        echo "<p>$number</p>";
+
+        // add 1 to counter
+        $number = $number + 1;
+
+
+      } while ($number <= 100);
+
+     ?>
+  </body>
+</html>
