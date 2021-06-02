8386be78ac18cc84b8287882f01d3185836ffa0b
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2021-06-02 21:38:31 +1200

    FizzBuzz
    If divisible by 3 but not 5 displays Fizz

--- php-intro/fizzbuzz.php
+++ php-intro/fizzbuzz.php
@@ -13,8 +13,17 @@
       do {
         // display current value
 
+        // divisible by both 5 and 3
         if ($number % 5 == 0 AND $number % 3 == 0) {
           echo "<p>FizzBuzz</p>";
+        }
+        // divisible by 5 but not 3
+        elseif ($number % 5 == 0) {
+          echo "<p>Buzz</p>";
+        }
+        // divisible by 3 but not 5
+        elseif ($number % 3 == 0) {
+          echo "<p>Fizz</p>";
         }
         else {
           echo "<p>$number</p>";
