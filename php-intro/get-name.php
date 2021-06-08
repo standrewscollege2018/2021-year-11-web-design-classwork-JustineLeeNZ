4fbcdafefa623f8a87f016a4a3d116115aa6fa75
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2021-06-08 22:23:28 +1200

    get-name
    made sure text was correct.
    pointed post at correct file

--- php-intro/get-name.php
+++ php-intro/get-name.php
@@ -5,8 +5,8 @@
     <title>Get a name</title>
   </head>
   <body>
-    <form action="check.php" method="post">
-      <p>Guess my name:</p>
+    <form action="greeting.php" method="post">
+      <p>Type in a name:</p>
       <input type="text" name="name-entered" placeholder="Enter a name">
       <button type="submit" name="submit">Go!</button>
 
