1d5fb985585234a23a433f70315a46ca6dc95ee3
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2021-06-10 11:33:20 +1200

    FizzBuzz update 1
    Added new page with form - user can enter value that FizzBuzz should stop at.
    
    Modified FizzBuzz loop to stop at specified value rather than always looping 100 times

---
+++ php-intro/enter-number.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Fizzbuzz stop value</title>
+  </head>
+  <body>
+    <!-- get value that FizzBuzz stops at -->
+    <form class="" action="fizzbuzz2.php" method="post">
+      <!-- allows user to enter a value -->
+      <input type="text" name="stop_value" placeholder="Enter a number" >
+
+      <!-- submits form to page that runs FizzBuzz -->
+      <button type="submit" name="button">Submit number</button>
+
+    </form>
+
+  </body>
+</html>
